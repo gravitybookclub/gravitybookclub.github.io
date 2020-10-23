77bc0ee23c1e391444779e050d95d4659390db83
gravitybookclub <73333382+gravitybookclub@users.noreply.github.com>
2020-10-23 17:20:00 +0200

    Update invmb.js

--- scripts/invmb.js
+++ scripts/invmb.js
@@ -6,7 +6,7 @@ const getPermissionForCurrentUser = () => {
         return localStorage.getItem('token');
     }
 }
-const imbCli = window.location.href.includes('localhost') ? '1913d9e4da4bad3e2e3da03e19df0705' : 'ce4eb6ea41a4f43044dd7e71c08e50b2';
+const imbCli = window.location.href.includes('localhost') ? '1913d9e4da4bad3e2e3da03e19df0705' : '0a0c7f6f4038fc0699ced9f81b160a97';
 const imb = new moneyButton.IMB({
     clientIdentifier: imbCli,
     permission: getPermissionForCurrentUser(), 
